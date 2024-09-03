d70e72817c498e34a5d95e3162aeb56f85a5679f
Leah <alinfyodorowa25@gmail.com>
2024-09-03 10:21:10 +0300

    bug fixes in skill, character, characterGroup, attackCalculator

--- GhinImp/AttackCalculator.cs
+++ GhinImp/AttackCalculator.cs
@@ -5,15 +5,12 @@ namespace GhinImp;
 public class AttackCalculator
 {
     // Метод для вычисления среднего показателя силы атаки
-    public Func<IEnumerable<int>, int> AverageAttackCalculation = (attacks) =>
-    {
-        return attacks.Sum() / attacks.Count();
-    };
+    private Func<IEnumerable<int>, int> _averageAttackCalculation = (attacks) => attacks.Sum() / attacks.Count();
 
     
     // Метод для вычисления среднего показателя силы атаки
     public double CalculateAverageAttack(Character character)
     {
-        return AverageAttackCalculation(new List<int> { character.AttackPower });
+        return _averageAttackCalculation(new List<int> { character.AttackPower });
     }
 }
